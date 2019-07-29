be7d850b24718e788c27358bc9a5d5070335ba56
iamtushara <tushar_anand@yahoo.com>
2019-07-30 00:26:39 +0530

    Update polynomial_regression.py
    Corrections to the code.

--- Machine Learning/1. Supervised Learning/1. Regression/3. Polynomial Regression/Codes/polynomial_regression.py
+++ Machine Learning/1. Supervised Learning/1. Regression/3. Polynomial Regression/Codes/polynomial_regression.py
@@ -16,20 +16,20 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 
 # Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+linear_reg = LinearRegression()
+linear_reg.fit(X, y)
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
-X_poly = poly_reg.fit_transform(X)
-poly_reg.fit(X_poly, y)
-lin_reg_2 = LinearRegression()
-lin_reg_2.fit(X_poly, y)
+poly_features = PolynomialFeatures(degree = 4)
+X_poly = poly_features.fit_transform(X)
+#poly_reg.fit(X_poly, y)
+polynomial_regressor = LinearRegression()
+polynomial_regressor.fit(X_poly, y)
 
 # Visualising the Linear Regression results
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.plot(X, linear_reg.predict(X), color = 'blue')
 plt.title('Truth or Bluff (Linear Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -37,7 +37,7 @@ plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.plot(X, polynomial_regressor.predict(poly_features.fit_transform(X)), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
@@ -47,14 +47,19 @@ plt.show()
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.plot(X_grid, polynomial_regressor.predict(poly_features.fit_transform(X_grid)), color = 'blue')
 plt.title('Truth or Bluff (Polynomial Regression)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
+# Question
+question = 6.5
+
 # Predicting a new result with Linear Regression
-lin_reg.predict(6.5)
+question_transformed = np.array([question]).reshape(-1, 1)
+linear_reg.predict(question_transformed)
 
 # Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
+question_transformed = poly_features.fit_transform(np.array([question]).reshape(-1, 1))
+polynomial_regressor.predict(question_transformed)
